3d5dd974b99898bfc5110b401afeb1875b83a697
OTMANE ER-RAGRAGUI <otmane.erragragui@gmail.com>
2022-08-22 13:36:51 +0200

    22.08.2022

--- Algo1/Solution.java
+++ Algo1/Solution.java
@@ -1413,10 +1413,18 @@ public class Solution {
             Arrays.fill(arr, 1);
         for (int col = 1; col < m; ++col) {
             for (int row = 1; row < n; ++row) {
-                dp[col][row] = dp[col-1][row]+dp[col][row-1];
+                dp[col][row] = dp[col - 1][row] + dp[col][row - 1];
             }
         }
-        return dp[m-1][n-1];
+        return dp[m - 1][n - 1];
+    }
+
+    public int characterReplacement(String s, int k) {
+        int ans = 0;
+        int[] ans = new int[26];
+
+
+        return ans;
     }
 
 }
