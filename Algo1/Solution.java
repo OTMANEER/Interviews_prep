017e1e63629c57b90eeae38f0277a3c47443326d
OTMANE ER-RAGRAGUI <otmane.erragragui@gmail.com>
2022-08-22 15:57:02 +0200

    22.08.2022

--- Algo1/Solution.java
+++ Algo1/Solution.java
@@ -1440,4 +1440,48 @@ public class Solution {
 
     }
 
+    public String getHint(String secret, String guess) {
+        HashMap<Character, Integer> h = new HashMap();
+        for (char s : secret.toCharArray()) {
+            h.put(s, h.getOrDefault(s, 0) + 1);
+        }
+
+        int bulls = 0, cows = 0;
+        int n = guess.length();
+        for (int idx = 0; idx < n; ++idx) {
+            char ch = guess.charAt(idx);
+            if (h.containsKey(ch)) {
+                // corresponding characters match
+                if (ch == secret.charAt(idx)) {
+                    // update the bulls
+                    bulls++;
+                    // update the cows
+                    // if all ch characters from secret
+                    // were used up
+                    if (h.get(ch) <= 0)
+                        cows--;
+                    // corresponding characters don't match
+                } else {
+                    // update the cows
+                    if (h.get(ch) > 0)
+                        cows++;
+                }
+                // ch character was used
+                h.put(ch, h.get(ch) - 1);
+            }
+        }
+
+        return Integer.toString(bulls) + "A" + Integer.toString(cows) + "B";
+    }
+
+    public String build(String S) {
+        Stack<Character> ans = new Stack();
+        for (char c : S.toCharArray()) {
+            if (c != '#')
+                ans.push(c);
+            else if (!ans.empty())
+                ans.pop();
+        }
+        return String.valueOf(ans);
+    }
 }
