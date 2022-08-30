151230311a5799f94a58c677214cacc35e606846
OTMANE ER-RAGRAGUI <62797836+OTMANEER@users.noreply.github.com>
2022-08-30 14:47:13 +0200

    Update revisions.java

--- DS_ALGO/revisions.java
+++ DS_ALGO/revisions.java
@@ -1,54 +1,48 @@
-// In Java, a Heap is represented by a Priority Queue
-import java.util.Collections;
+// Code for Min Heap
 import java.util.PriorityQueue;
-import java.util.Arrays;
 
-// Construct an empty Min Heap
-PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-
-// Construct an empty Max Heap
-PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
-
-// Construct a Heap with initial elements. 
-// This process is named "Heapify".
-// The Heap is a Min Heap
-PriorityQueue<Integer> heapWithValues= new PriorityQueue<>(Arrays.asList(3, 1, 2));
-
-// Insertion in Heaps
-
-// time Complexity is O(log(n))
-
-// Space Complexity O(1)
-
-// Insert an element to the Min Heap
-minHeap.add(5);
-
-// Insert an element to the Max Heap
-maxHeap.add(5);
-
-
-// get the Top element 
-
-// space and time remain the same  O(1)
-
-heap.peek();
-
-// Delete the top element in O(log(n))
-
-// Delete top element from the Min Heap
-minHeap.poll();
-
-// Delete top element from the Max Heap
-maxheap.poll();
-
-
-// Length of the Min Heap
-minHeap.size();
-
-// Length of the Max Heap
-maxHeap.size();
-
-// Note, in Java, apart from checking if the length of the Heap is 0, we can also use isEmpty()
-// If there are no elements in the Heap, isEmpty() will return true;
-// If there are still elements in the Heap, isEmpty() will return false;
-
+public class App {
+    public static void main(String[] args) {
+        // Construct an instance of Min Heap
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        
+        // Add 3，1，2 respectively to the Min Heap
+        minHeap.add(3);
+        minHeap.add(1);
+        minHeap.add(2);
+        
+        // Check all elements in the Min Heap, the result is [1, 3, 2]
+        System.out.println("minHeap: " + minHeap.toString());
+        
+        // Get the top element of the Min Heap
+        int peekNum = minHeap.peek();
+        
+        // The result is 1
+        System.out.println("peek number: " + peekNum);
+        
+        // Delete the top element in the Min Heap
+        int pollNum = minHeap.poll();
+        
+        // The reult is 1
+        System.out.println("poll number: " + pollNum);
+        
+        // Check the top element after deleting 1, the result is 2
+        System.out.println("peek number: " + minHeap.peek());
+        
+        // Check all elements in the Min Heap, the result is [2,3]
+        System.out.println("minHeap: " + minHeap.toString());
+        
+        // Check the number of elements in the Min Heap
+        // Which is also the length of the Min Heap
+        int heapSize = minHeap.size();
+        
+        // The result is 2
+        System.out.println("minHeap size: " + heapSize);
+        
+        // Check if the Min Heap is empty
+        boolean isEmpty = minHeap.isEmpty();
+        
+        // The result is false
+        System.out.println("isEmpty: " + isEmpty);
+    }
+}
